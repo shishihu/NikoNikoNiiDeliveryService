98c493b2ec59cd020a9b274733762de800194d57
Shishi <jasinayu7@gmail.com>
2025-04-08 15:52:45 -0400

    meow

---
+++ Restaurants.java
@@ -0,0 +1,14 @@
+public interface Restaurants{
+    public int cost;
+    public List<String> menuItems;
+    public String cuisine;
+    public int getCost(){
+        return cost;
+    }
+    public List<String> getMenuItems(){
+        return menuItems;
+    }
+    public getCuisine(){
+        return cuisine;
+        }
+}
---
+++ TheGoldenWhisk.java
@@ -0,0 +1,7 @@
+public class TheGoldenWhisk implements Restaurants{
+    cuisine = "Chinese";
+    List<String> menu = ["Spring Rolls", "Dumplings", "Crab Rangoon",
+        "Sweet and Sour Chicken", "Kung Pao Pork", "Beef with Broccoli", "General Tso's Chicken"];
+    List<Double> costs = [5.0, 10.0, 3.0, 5.0, 6.0, 7.0, 5.0];
+
+}
