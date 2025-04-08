a0ee6c9ecc8f4ceffaaa0784a807f7567d92c5c5
racoltdev <racoltdev@gmail.com>
2025-04-08 17:06:47 -0400

    Cli uses Lists. Numbers for selections are bolded

--- Cli.java
+++ Cli.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 class Cli {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 
 		System.out.println("Select a cuisine: ");
-		String[] cuisines = getCuisines();
+		List<String> cuisines = getCuisines();
 		int cuisineIndex = makeSelection(cuisines, in);
-		System.out.println("User selected " + cuisines[cuisineIndex]);
+		System.out.println("User selected " + cuisines.get(cuisineIndex));
 
-		String[] restaurants = getRestaurants(cuisines[cuisineIndex]);
+		List<String> restaurants = getRestaurants(cuisines.get(cuisineIndex));
 		System.out.println("Select a restaurant: ");
 		int restaurantIndex = makeSelection(restaurants, in);
-		System.out.println("User selected " + restaurants[restaurantIndex]);
+		System.out.println("User selected " + restaurants.get(restaurantIndex));
 
 		in.close();
 	}
 
-	private static int makeSelection(String[] list, Scanner in) {
-		for (int i = 0; i < list.length; i++) {
-			System.out.println(i + 1 + ": " + list[i]);
+	private static int makeSelection(List<String> list, Scanner in) {
+		for (int i = 0; i < list.size(); i++) {
+			// This uses ANSII color codes
+			// Set number bold, then reset
+			System.out.println("\t\033[1;37m" + (i + 1) + ":\033[0m " + list.get(i));
 		}
 
 		int index = 0;
@@ -27,7 +32,7 @@ class Cli {
 			String selection = in.nextLine();
 			try {
 				index = Integer.valueOf(selection) - 1;
-				if (index < 0 || index >= list.length) {
+				if (index < 0 || index >= list.size()) {
 					System.out.println("Invalid selection. Try again.");
 					index = 0;
 				}
@@ -38,14 +43,15 @@ class Cli {
 		return index;
 	}
 
-	private static String[] getCuisines() {
-		return new String[]{"joe", "biden", "niko", "nii"};
+	private static List<String> getCuisines() {
+		List<String> c = Arrays.asList("joe", "biden", "niko", "nii");
+		return c;
 	}
 
-	private static String[] getRestaurants(String cuisine) {
-		String[] restaurants = new String[10];
-		for (int i = 0; i < restaurants.length; i++) {
-			restaurants[i] = Integer.toString(i);
+	private static List<String> getRestaurants(String cuisine) {
+		List<String> restaurants = new ArrayList<String>();
+		for (int i = 0; i < 10; i++) {
+			restaurants.add(Integer.toString(i));
 		}
 		return restaurants;
 	}
