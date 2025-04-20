439ee7895a2980d6111ea0cae392f7c95e73b394
Shishi <jasinayu7@gmail.com>
2025-04-20 17:49:11 -0400

    FC updates

--- FoodCustomization.java
+++ FoodCustomization.java
@@ -4,22 +4,17 @@ public class FoodCustomization extends Food{
     public FoodCustomization(String name, int cost){
         super(name, cost);
         Scanner sf = new Scanner(System.in);
-        while (true) {
-            System.out.println("Would you like to customize your dish?");
-            String yesOrNo = sf.nextLine();
-            if (yesOrNo.charAt(0) == 'y') {
-                System.out.println("What customizations would you like?");
-                String cs = sf.nextLine();
-                String newName = getItemWithCustomizations(cs);
-                setName(newName);
-            }
-            else{
-                break;
-            }
+        System.out.println("Would you like to customize your dish?");
+        String yesOrNo = sf.nextLine();
+        if (yesOrNo.charAt(0) == 'y') {
+            System.out.println("What customizations would you like?");
+            String cs = sf.nextLine();
+            String newName = getItemWithCustomizations(cs);
+            setName(newName);
         }
         System.out.println("You have ordered " + getName());
     }
     public String getItemWithCustomizations(String customizations){
-        return getName() + "with" + customizations;
+        return getName() + " with " + customizations;
     }
 }
