def6852c96a458dcc63a4ab27283839454ed81e8
Shishi <jasinayu7@gmail.com>
2025-04-20 16:52:28 -0400

    food

--- Food.java
+++ Food.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Food {
+import java.util.List;
+
+public class Food {
+    private String name;
+    public List<String> customizations;
+
+    public String getName(){
+        return this.getName();
+    }
 }
--- FoodCustomization.java
+++ FoodCustomization.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class FoodCustomization {
+import java.util.Scanner;
+
+public class FoodCustomization extends Food{
+    public FoodCustomization(){
+        String customizations = "";
+        System.out.println("Would you like to customize your dish?");
+        Scanner sf = new Scanner(System.in);
+        String yesOrNo = sf.nextLine();
+        if (yesOrNo.charAt(0) == 'y'){
+            System.out.println("What customizations would you like?");
+        } else if (yesOrNo.charAt(0) == 'n'){
+
+        }
+        else{
+
+        }
+    }
 }
